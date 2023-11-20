961eac221b41aaef24a6c3f28995513cea3988b4
Patrick5432 <112853910+Patrick5432@users.noreply.github.com>
2023-11-20 19:46:31 +0500

    Добавление кода в file_sharing, где в html добавляются теги с названием файлов

--- client/main-page/file_sharing.js
+++ client/main-page/file_sharing.js
@@ -3,3 +3,17 @@ const userName = document.querySelector(".user-name")
 
 userName.innerHTML = `
 <span id="user-name">${UserName}</span>`
+
+const form = document.querySelector(".upload_files")
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const files = document.getElementById("upload_file").files
+
+    const addFiles = document.querySelector(".files")
+
+    for (let i = 0; i < files.length; i++) {
+        const fileTag = document.createElement("p")
+        fileTag.textContent = files[i].name
+        addFiles.appendChild(fileTag)
+    }
+})
